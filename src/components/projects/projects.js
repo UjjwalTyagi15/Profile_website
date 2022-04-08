5049b6595d0d2277e9e30682a9fd45cbc4343c37
UjjwalTyagi15e <ujjwaltyagi1512@gmail.com>
2022-04-09 00:52:35 +0530

    new tab for links

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import './projects.css'
 const Projects =({brain , name, link})=>{
-return (<a href={link}>
+return (<a href={link} target='_blank' >
 
     <div className='ba pointer bw2 tc grow  br3 pa3 ma2 dib bw2 shadow-5 project '>
         <img className='ba pa2 projecticon' style={{ height: 250 , width: 250 }} src={`${brain}?size=200x200`} alt="smart-brain.png" ></img>
